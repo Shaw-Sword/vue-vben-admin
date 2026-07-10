a73f22bbd721551dcbd56bd861e63622b06fbf3d
Shaw1162084 <1162084@qq.com>
2026-07-11 01:54:02 +0800

    fix: shaw-添加一个路由,国际化配置,页面

---
+++ apps/web-naive/src/router/routes/modules/shaw-demo.ts
@@ -0,0 +1,73 @@
+import type { RouteRecordRaw } from 'vue-router';
+
+import { $t } from '#/locales';
+
+const routes: RouteRecordRaw[] = [
+  {
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: $t('shaw.title'),
+    },
+    name: 'Demos-1',
+    path: '/demos-1',
+    children: [
+      {
+        meta: {
+          title: $t('demos.naive'),
+        },
+        name: 'NaiveDemos-1',
+        path: 'naive-1',
+        component: () => import('#/views/demos/naive/index.vue'),
+      },
+      {
+        meta: {
+          title: $t('demos.table'),
+        },
+        name: 'Table-1',
+        path: 'table-1',
+        component: () => import('#/views/demos/table/index.vue'),
+      },
+      {
+        meta: {
+          title: $t('demos.form'),
+        },
+        name: 'Form-1',
+        path: 'form-1',
+        component: () => import('#/views/demos/form/basic.vue'),
+      },
+      {
+        meta: {
+          title: $t('demos.arrayForm'),
+        },
+        name: 'ArrayForm-1',
+        path: 'array-form-1',
+        component: () => import('#/views/demos/naive/array-form/index.vue'),
+      },
+    ],
+  },
+  {
+    meta: {
+      icon: 'ic:baseline-view-in-ar',
+      keepAlive: true,
+      order: 1000,
+      title: $t('shaw.test1.title'),
+    },
+    name: 'Demos-2',
+    path: '/demos-2',
+    children: [
+      {
+        meta: {
+          title: $t('shaw.test1.sub_title'),
+          icon: 'mdi:home',
+        },
+        name: 'table-2',
+        path: 'table-2',
+        component: () => import('#/views/shaw/table/index.vue'),
+      },
+    ],
+  },
+];
+
+export default routes;
---
+++ apps/web-naive/src/views/shaw/table/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+
+const columns = ref([
+  {
+    key: 'no',
+    title: 'No',
+  },
+  {
+    key: 'title',
+    title: 'Title',
+  },
+  {
+    key: 'length',
+    title: 'Length',
+  },
+]);
+const data = [
+  { length: '4:18', no: 3, title: 'Wonderwall' },
+  { length: '4:48', no: 4, title: "Don't Look Back in Anger" },
+  { length: '7:27', no: 12, title: 'Champagne Supernova' },
+];
+// console.log(columns, data);
+</script>
+
+<template>
+  <!-- <Page
+    description="表单页用于向用户收集或验证信息，基础表单常见于数据项较少的表单场景。"
+    title="NDataTable"
+  >
+    <NDataTable :columns="columns" :data="data" >
+      <template #loading></template>
+    </NDataTable>
+  </Page> -->
+  <Page>
+    <div>1</div>
+    <div>{{ columns[0]?.key }},{{ data[0]?.title }}</div>
+  </Page>
+</template>
+
+<style scoped></style>
